5499dd69d9a8a7780d4a8211700040faf38cc56a
Orteku <Orteku@192.168.1.41>
2023-08-02 23:33:18 +0200

    Terminado

--- src/funciones/Utilidades.java
+++ src/funciones/Utilidades.java
@@ -17,10 +17,29 @@ public class Utilidades {
 	 * @throws ArithmeticException cuando la nota no está entre 0 y 10
 	 */
 	public String devuelveNota(double nota) {
-		return null;
+		if (nota<0 || nota>10) {
+			throw new ArithmeticException();
+		} else if (nota<5) {
+			return "Suspenso";
+		} else if (nota<6.5) {
+			return "Bien";
+		} else if (nota<8.5) {
+			return "Notable";
+		} else if (nota<10) {
+			return "Sobresaliente";
+		} else {
+			return "Matrícula";
+		}
 	}
 	
-	
+	/**
+	 * Método que calcula el salario correspondiente a las horas extras realizadas.
+	 * @param numHoras int Cantidad de horas realizadas
+	 * @param precioHoras double Precio al que se paga cada hora trabajada
+	 * @param precioExtras double Precio al que se paga cada hora extra
+	 * @return double Salario total contando las horas extras
+	 * @throws ArithmeticException cuando se introducen valores negativos o 0
+	 */
 	public double calculaSalario(int numHoras, double precioHoras, double precioExtras) {
 		int extras=0;
 		int horas=numHoras;
@@ -34,7 +53,11 @@ public class Utilidades {
 		return horas*precioHoras+extras*precioExtras;		
 	}
 	
-
+	/**
+	 * Método que cuenta cuántos divisores tiene el número introducido
+	 * @param num int Número del que queremos contar sus divisores
+	 * @return int Cantidad de divisores del número introducido
+	 */
 	public int cuentaDivisores(int num) {
 		int cuenta=0;
 		
@@ -53,9 +76,22 @@ public class Utilidades {
 	 * @return int con el menor número del vector.
 	 */
 	public double devuelveMenor(int v[]) {
-		return 0;
+		// Guardo en menor el primer valor del vector
+		int menor = v[0];
+		// Como está guardado el primer valor (0), empiezo el bucle desde el segundo (1)
+		for (int i=1;i<v.length;i++) {
+			// Uso la comparación de menores con el valor guardado y la posición del vector,
+			// por lo que si la posición es menor se sustuiturá en la variable.
+			menor=Math.min(menor, v[i]);
+		}
+		return menor;
 	}
 	
+	/**
+	 * Método que ordena los valores de un vector de menor a mayor
+	 * @param v in[] vector a ordenar
+	 * @return in[] con el vector ordenado
+	 */
 	public int [] burbuja(int[] v)
     {
       int auxiliar;
--- test/funciones/UtildadesTest.java
+++ test/funciones/UtildadesTest.java
@@ -1,6 +1,8 @@
 package funciones;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -42,11 +44,39 @@ private static Utilidades utils;
 
 	@Test
 	void testCalculaSalario() {
+		// Como al método al que se llama no es 'static' es necesario instanciar la clase para poder llamarlo
+		utils = new Utilidades();
+
 		
+		// Test con horas extras
+		assertEquals(utils.HORASJORNADA*20+(40-utils.HORASJORNADA)*35,
+				utils.calculaSalario(40,20,35));
+		
+		//Test sin horas extras
+		assertEquals(20*20,utils.calculaSalario(20,20,35));
+		
+		//Test límite, con las hora a 38
+		assertEquals(38*20,utils.calculaSalario(38,20,35));
+		
+		//Test de errores con valores negativos y 0
+		assertThrows(ArithmeticException.class,()->utils.calculaSalario(0, 20, 35));
+		assertThrows(ArithmeticException.class,()->utils.calculaSalario(40, 0, 35));
+		assertThrows(ArithmeticException.class,()->utils.calculaSalario(40, 20, 0));
+		assertThrows(ArithmeticException.class,()->utils.calculaSalario(-10, 20, 35));
+		assertThrows(ArithmeticException.class,()->utils.calculaSalario(40, -10, 35));
+		assertThrows(ArithmeticException.class,()->utils.calculaSalario(35, 20, -10));
+
 	}
 
 	@Test
 	void testCuentaDivisores() {
+		//Test con 10
+		assertEquals(4,utils.cuentaDivisores(10));
+		//Test con 0
+		assertEquals(0,utils.cuentaDivisores(0));
+		//Test con negativos
+		assertEquals(0,utils.cuentaDivisores(-10));
+		
 
 	}
 
@@ -61,7 +91,18 @@ private static Utilidades utils;
 
 	@Test
 	void testBurbuja() {
-
+		int v[] = {3,5,1,4,2};
+		int ordenado[] = {1,2,3,4,5};
+		
+		// Para comparar vectores se tiene que usar este método.
+		// Es posible que Eclipse no lo importe bien y tengas que ir a la parte superior
+		// a ponerlo a mano.
+		assertArrayEquals(ordenado,utils.burbuja(v));
+		
+		// Test con negativos
+		int v2[] = {3,-5,1,4,2};
+		int ordenado2[] = {-5, 1,2,3,4};
+		assertArrayEquals(ordenado2,utils.burbuja(v2));
 	}
 
 }
